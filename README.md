43fe403d581bfddb06f7ac1894ee346f1e46f862
jacoblee93 <jacoblee93@gmail.com>
2023-11-06 16:51:41 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Multimodal GPT-4V Not Hotdog
+
+OpenAI had a ton of awesome announcements at their most recent Developer Day, including multimodal support.
+
+I immediately seized the opportunity to use billions of dollars of hard work and AI research to create the ultimate product:
+a [Twilio](https://twilio.com) powered app that can detect whether an image contains a hotdog or not!
+
+It leverages GPT-4V, [LangChain.js](https://github.com/langchain-ai/langchainjs), and Twilio. The live deployment runs on a Cloudflare worker.
+
+## Setup
+
+First, you'll need to claim a phone number with MMS capability from [Twilio](https://twilio.com).
+
+You'll need to set the following environment variables in `.dev.vars` and in your Cloudflare console:
+
+```
+OPENAI_API_KEY=
+TWILIO_ACCOUNT_SID=
+TWILIO_AUTH_TOKEN=
+```
+
+Run `npm install`. You can run the `src/index.ts` file locally with `npx wrangler dev`, but it's easiest to test with a live Twilio number.
+
+When you're ready, deploy with `npx wrangler deploy`. Take note of the URL and put it as the recipient webhook in Twilio.
+
+And that's it! Text your phone number a picture, and it will respond with whether it's a hotdog or not.
+
+## Thank you!
+
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,103 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { HumanMessage } from "langchain/schema";
+import { ChatPromptTemplate, MessagesPlaceholder } from "langchain/prompts";
+import { StringOutputParser } from "langchain/schema/output_parser";
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+  OPENAI_API_KEY: string;
+  TWILIO_ACCOUNT_SID: string;
+  TWILIO_AUTH_TOKEN: string;
+}
+
+/**
+ * readRequestBody reads in the incoming request body
+ * Use await readRequestBody(..) in an async function to get the string
+ * @param {Request} request the incoming request to read from
+ */
+async function readRequestBody(request: Request): Promise<Record<string, any>> {
+  const contentType = request.headers.get("content-type") ?? "";
+  if (contentType.includes("application/json")) {
+    return request.json() as any;
+  } else if (contentType.includes("form")) {
+    const formData = await request.formData();
+    const body: Record<string, any> = {};
+    for (const entry of formData.entries()) {
+      body[entry[0] as string] = entry[1];
+    }
+    return body;
+  } else {
+    throw new Error("Invalid content type header.");
+  }
+}
+
+const sendRequest = async ({ to, from, body, accountSid, authToken }: { to: string, from: string, body: string, accountSid: string, authToken: string }) => {
+  const url = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`;
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': `Basic ${btoa(accountSid + ':' + authToken)}`
+    },
+    body: new URLSearchParams({
+      'To': to,
+      'From': from,
+      'Body': body
+    }).toString()
+  });
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return response.json();
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+    const chat = new ChatOpenAI({
+      openAIApiKey: env.OPENAI_API_KEY,
+      modelName: "gpt-4-vision-preview",
+    });
+    const body = await readRequestBody(request);
+    const { From: originNumber, To: recipientNumber, MediaUrl0: imageUrl } = body;
+    if (!imageUrl) {
+      await sendRequest({
+        body: `Send a picture and I'll tell you whether it's a hotdog or not!`,
+        to: originNumber,
+        from: recipientNumber,
+        accountSid: env.TWILIO_ACCOUNT_SID,
+        authToken: env.TWILIO_AUTH_TOKEN,
+      });
+      throw new Error("No image url found in incoming message.");
+    }
+    const prompt = ChatPromptTemplate.fromMessages([
+      ["system", `You are an image classifer that only knows two words: "Yes" or "No". Answer the user question with only a one word answer. Do not respond with anything else.`],
+      new MessagesPlaceholder("input"),
+    ]);
+    const chain = prompt.pipe(chat).pipe(new StringOutputParser());
+    const result = await chain.invoke({
+      input: [
+        new HumanMessage({
+          content: [
+            {
+              type: "text",
+              text: "Is this image a hotdog?"
+            },
+            {
+              type: "image_url",
+              image_url: imageUrl
+            },
+          ],
+        }),
+      ],
+    });
+    console.log("RESULT", result);
+    const responseText = result === "Yes" ? `✅ Hotdog` : `❌ Not Hotdog`;
+    await sendRequest({
+      body: responseText,
+      to: originNumber,
+      from: recipientNumber,
+      accountSid: env.TWILIO_ACCOUNT_SID,
+      authToken: env.TWILIO_AUTH_TOKEN,
+    });
+		return new Response('OK');
 	},
 };
